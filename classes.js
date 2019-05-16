6aba31e24986e35a3586f1f1c8ef0153f9204c02
Nikola Todorovski <todorovski_nikola@hotmail.com>
2019-05-16 11:44:58 +0200

    update

---
+++ classes.js
@@ -0,0 +1,10 @@
+class Phone {
+    constructor(obj){
+        let {name,price,img,phone_url,specifications} = obj;
+        this.name = name;
+        this.price = price;
+        this.img = img;
+        this.phone_url = phone_url;
+        this.specifications = specifications;
+    }
+}
